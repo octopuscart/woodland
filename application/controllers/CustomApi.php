5ac7015d45eba6ccf119266b408cdcac2cbd2856
dev1 <dev@octopuscart.com>
2018-09-21 10:34:37 +0530

    changes

--- application/controllers/CustomApi.php
+++ application/controllers/CustomApi.php
@@ -3,7 +3,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 require(APPPATH . 'libraries/REST_Controller.php');
 
-class customApi extends REST_Controller {
+class CustomApi extends REST_Controller {
 
     public function __construct() {
         parent::__construct();

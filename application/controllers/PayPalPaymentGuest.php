29186811cb0b832f6ef31f793b59e881104d0784
dev1 <dev@octopuscart.com>
2018-09-23 18:09:46 +0530

    changes

--- application/controllers/PayPalPaymentGuest.php
+++ application/controllers/PayPalPaymentGuest.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class PayPalPayment extends CI_Controller {
+class PayPalPaymentGuest extends CI_Controller {
 
     public function __construct() {
         parent::__construct();

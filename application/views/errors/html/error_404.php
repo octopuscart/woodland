0034ab29682845f986bcf026ecff416b2f6fd7cc
dev1 <dev@octopuscart.com>
2018-09-26 16:50:11 +0530

    changes in 404

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,25 +1,26 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <?php
-//$this->load->view('layout/header');
+$ci = new CI_Controller();
+$ci = & get_instance();
+$ci->load->helper('url');
 ?>
-
+<?php
+include APPPATH . '/views/layout/header.php';
+?>
 <!-- Content -->
 <div id="content"> 
     <!-- Tesm Text -->
-    <section class="error-page text-center pad-t-b-130">
-        <div class="container "> 
+    <section class="page_title bg_gradiant image_fixed t_align_c relative wrapper" style="margin-top: 0px;">
+        <img src="images/page_404.jpg" alt="" class="page_404 d_xs_none">
+        <h1 class="color_light fw_light m_bottom_12">Error 404 Page</h1>
+        <!--breadcrumbs-->
+        <p class="color_grey_light_3" style="margin-bottom: 50px;">This Page Could Not Be Found :(</p>
+        <a href="/"  style="    font-size: 20px;" class="button_type_3 color_pink r_corners tt_uppercase fs_medium tr_all  m_right_10 m_md_bottom_10">BACK TO HOME</a>
 
-            <!-- Heading -->
-            <h1>404</h1>
-            <p>We're sorry, the page you requested cannot be found.<br>
-                You can go back to</p>
-            <hr class="dotted">
-            <a href="http://classapartstore.com" class="btn btn-inverse">BACK TO HOME</a>
-        </div>
     </section>
+
 </div>
 <!-- End Content --> 
-
-
 <?php
-//$this->load->view('layout/footer');
+include APPPATH . '/views/layout/footer.php';
 ?>

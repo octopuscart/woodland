e33c3577ea59398666fc8574af045570b8212599
dev1 <dev@octopuscart.com>
2018-09-26 17:13:19 +0530

    changes in 404

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -4,21 +4,21 @@ $ci = new CI_Controller();
 $ci = & get_instance();
 $ci->load->helper('url');
 ?>
+
 <?php
-include APPPATH . '/views/layout/header.php';
+include APPPATH . '/views/layout/header_2.php';
 ?>
 <!-- Content -->
 <div id="content"> 
-    <!-- Tesm Text -->
     <section class="page_title bg_gradiant image_fixed t_align_c relative wrapper" style="margin-top: 0px;">
         <img src="images/page_404.jpg" alt="" class="page_404 d_xs_none">
-        <h1 class="color_light fw_light m_bottom_12">Error 404 Page</h1>
+        <h1 class="color_light fw_light m_bottom_12 text-center">Error 404 Page</h1>
         <!--breadcrumbs-->
-        <p class="color_grey_light_3" style="margin-bottom: 50px;">This Page Could Not Be Found :(</p>
-        <a href="/"  style="    font-size: 20px;" class="button_type_3 color_pink r_corners tt_uppercase fs_medium tr_all  m_right_10 m_md_bottom_10">BACK TO HOME</a>
-
+        <h4 class="color_grey_light_3 text-center" style="margin-bottom: 50px; ">This Page Could Not Be Found :(</h4>
+       <center style="margin-bottom: 50px; ">
+           <a href="/"  style="    font-size: 20px;" class=" text-center btn btn-danger">BACK TO HOME</a>
+       </center>
     </section>
-
 </div>
 <!-- End Content --> 
 <?php

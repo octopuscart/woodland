a41cf759dcb9a9bc5de0f001802c5380ccc33824
dev1 <dev@octopuscart.com>
2018-11-23 15:52:32 +0530

    chag

--- application/views/Product/customization_suit_v2.php
+++ application/views/Product/customization_suit_v2.php
@@ -273,7 +273,8 @@ $this->load->view('layout/header');
                                         <img src="<?php echo custome_image_server; ?>/jacket/output/{{screencustom.productobj.folder}}/{{img}}" class="fixpos animated" ng-repeat="img in selecteElements[fab.product_id]['Number of Pleat'].elements">
                                         <img src="<?php echo custome_image_server; ?>/jacket/overlay/{{img}}" class="fixpos animated" ng-repeat="img in selecteElements[fab.product_id]['Front Pocket Style'].elements">  
                                         <img src="<?php echo custome_image_server; ?>/jacket/output/{{screencustom.productobj.folder}}/{{img}}" class="fixpos animated" ng-repeat="img in selecteElements[fab.product_id]['Waistband'].elements">
-                                        <img src="<?php echo custome_image_server; ?>/jacket/overlay//{{img}}" class="fixpos animated" ng-repeat="img in selecteElements[fab.product_id]['Cuff'].elements">
+                                        <img src="<?php echo custome_image_server; ?>/jacket/overlay/{{img}}" class="fixpos animated" ng-repeat="img in selecteElements[fab.product_id]['Cuff'].elements">
+                                        <img src="<?php echo custome_image_server; ?>/jacket/overlay/zipper.png" class="fixpos animated" >
 
                                     </div> 
 

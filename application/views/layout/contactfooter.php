954340efb3804e09c1f580a82a0851b39820030b
dev <dev@develop.com>
2020-05-02 12:10:53 +0530

    changes

---
+++ application/views/layout/contactfooter.php
@@ -0,0 +1,79 @@
+<div class="container section-contact topmargin-lg clearfix footercontact" style="background:url(<?php echo base_url(); ?>assets/theme2/res/images/vegbackground.jpg)">
+
+    <div class="row clearfix" >
+        <div class="col-lg-3 col-md-6 bottommargin-sm center">
+            <i class="i-plain i-xlarge divcenter nobottommargin icon-et-map"></i>
+            <h3 class="uppercase font-body" style="font-size: 22px; font-weight: 700;margin-top: 20px">Contact</h3>
+            <span class="font-primary">
+                UG Shop 16 & 17, Wing On Plaza,<br/>
+                62, Mody Road, Tsim Sha Tsui East,<br/>
+                Kowloon, Hong Kong
+            </span>
+        </div>
+        <div class="col-lg-3 col-md-6 bottommargin-sm center">
+            <i class="i-plain i-xlarge divcenter nobottommargin icon-et-clock"></i>
+            <h3 class="uppercase font-body" style="font-size: 22px; font-weight: 700;margin-top: 20px">Opening Time</h3>
+            <span class="font-primary">
+                <table style="    width: 100%;text-align: left;">
+                    <tr>
+                        <td colspan="2" style="text-align: center;"><strong>Monday To Sunday</strong></td>
+
+                    </tr>
+                    <tr>
+                        <td>Lunch</td>
+                        <td>: &nbsp; 11:30 am – 03:30 pm</td>
+                    </tr>
+                    <tr>
+                        <td>Dinner</td>
+                        <td>: &nbsp; 06:30 pm – 10:30 pm</td>
+                    </tr>
+                </table>
+            </span>
+        </div>
+        <div class="col-lg-3 col-md-6 bottommargin-sm center">
+            <i class="i-plain i-xlarge divcenter nobottommargin icon-et-clipboard"></i>
+            <h3 class="uppercase font-body" style="font-size: 22px; font-weight: 700;margin-top: 20px">Reservation</h3>
+            <span class="font-primary">
+                <table style="    width: 100%;">
+                    <tr>
+                        <td colspan="1" style="text-align: center;font-size: 20px;"><strong>+(852) 2369 3718</strong></td>
+
+                    </tr>
+                    <tr>
+
+                        <td><i class="icon-et-phone"></i>&nbsp; +(852) 2366 1945</td>
+                    </tr>
+                    <tr>
+
+                        <td><i class="icon-et-envelope"></i>&nbsp; reachus@vegconcepts.com</td>
+                    </tr>
+                </table>
+            </span>
+        </div>
+        <div class="col-lg-3 col-md-6 bottommargin-sm center">
+            <i class="i-plain i-xlarge divcenter nobottommargin icon-et-heart"></i>
+            <h3 class="uppercase font-body" style="font-size: 22px; font-weight: 700;margin-top: 20px">Social Contact</h3>
+            <div style="display: flex; justify-content: center">
+                <a href="#" class="socialicon ">
+                    <i class="fa fa-facebook"></i>
+                </a>
+                <a href="#" class="socialicon ">
+                    <i class="fa fa-twitter"></i>
+                </a>
+                <a href="#" class="socialicon ">
+                    <i class="fa fa-instagram"></i>
+                </a>
+
+                <a href="#" class="socialicon ">
+                    <i class="fa fa-youtube"></i>
+                </a>
+
+
+                <a href="#" class="socialicon ">
+                    <i class="fa fa-tripadvisor"></i>
+                </a>
+
+            </div>
+        </div>
+    </div>
+</div>

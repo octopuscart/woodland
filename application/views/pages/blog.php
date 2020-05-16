558a07f392c6d0e9073e3a06fe623d95c958f952
dev <dev@develop.com>
2020-05-16 12:19:52 +0530

    cahnges

--- application/views/pages/blog.php
+++ application/views/pages/blog.php
@@ -6,13 +6,20 @@ $this->load->view('layout/header');
     footer{
         /*position: inherit!important;*/
     }
+    .entry-details {
+        padding: 15px;
+    }
+
+    .entry-details ul{
+        padding: 0px 15px;
+    }
 </style>
 
 
 
 <!-- Inner Page Banner Area Start Here -->
 <section id="page-title" class="page-title-parallax page-title-center border-bottom" style="background-image: url('<?php echo base_url(); ?>assets/theme2/res/images/sections/blog.jpg');   margin-top: -30px;        padding: 149px 0px;
-    background-position: -87px -70px;  " data-center="" data-top-bottom="">
+         background-position: -87px -70px;  " data-center="" data-top-bottom="">
     <div class="container clearfix">
         <h1 class="font-secondary capitalize ls0" style="font-size: 62px;">Our Blog</h1>
     </div>
@@ -28,46 +35,159 @@ $this->load->view('layout/header');
     <div class="content-wrap nobottompadding">
         <div class="container clearfix">
             <div class="row clearfix">
-                <div class="col-lg-6 col-sm-6 bottommargin-sm">
-                    <div class="ipost clearfix">
-                        <div class="entry">
-                            <div class="entry-image nobottommargin">
-                                <a href="<?php echo base_url(); ?>assets/theme2/res/images/blogs/2.jpg" data-lightbox="image"><img class="image_fade" src="<?php echo base_url(); ?>assets/theme2/res/images/blogs/2.jpg" alt="Image 1"></a>
+                <div class='col-md-6'>
+                    <div class="col-lg-12 col-sm-12 bottommargin-sm">
+
+                        <div class="ipost clearfix">
+                            <div class="entry">
+                                <div class="entry-image nobottommargin">
+                                    <a href="<?php echo base_url(); ?>assets/theme2/res/images/blogs/bread-upma_620x350_71522139974.jpg" data-lightbox="image"><img class="image_fade" src="<?php echo base_url(); ?>assets/theme2/res/images/blogs/bread-upma_620x350_71522139974.jpg" alt="Image 1"></a>
+                                </div>
+                                <div class="entry-title">
+                                    <ul class="entry-meta clearfix">
+                                        <li><a href="#" style="text-transform: capitalize;"><?php echo date("dS M Y"); ?></a></li>
+                                        <li><a href="#">ADMIN</a></li>
+                                    </ul>
+                                    <div class="clear"></div>
+                                    <h2 class="nobottommargin"><a href="#">Rawa Upma Recipe
+                                        </a>
+                                    </h2>
+                                </div>
+                                <div class="entry-details">
+                                    <p>
+                                        About Rawa Upma Recipe: A delicious south Indian breakfast dish made with semolina, vegetables and topped with some grated coconut
+                                    </p>
+                                    <h3>
+                                        How to Make Rawa Upma
+                                    </h3>
+                                    <ul>
+
+                                        <li> 1.Boil water.</li>
+                                        <li>2.Slice onions and cut the chillies lengthwise.</li>
+                                        <li> 3.Heat a deep pan with ghee. Add mustard, when crackling add the dals and sauté until golden.</li>
+                                        <li> 4.Add onions, curry leaves, cashew nuts and green chillies. When the onions are translucent add semolina and sauté.</li>
+                                        <li> 5.Add the boiling water and stir rapidly with the salt. When the moisture is absorbed, the upma is cooked.</li>
+                                        <li> 6.Mix well and garnish with grated coconut and coriander leaves.</li>
+                                    </ul>
+                                    <p>
+                                        <b>Key Ingredients:</b> Semolina, Water, Ghee, Mustard seeds, Black gram dal, Bengal gram dal, Onions, Green chillies, Cashew nuts, Curry leaves salt, Coriander leaves, Coconut
+                                    </p>
+                                </div>
                             </div>
-                            <div class="entry-title">
-                                <ul class="entry-meta clearfix">
-                                    <li><a href="#" style="text-transform: capitalize;"><?php echo date("dS M Y");?></a></li>
-                                    <li><a href="#">ADMIN</a></li>
-                                </ul>
-                                <div class="clear"></div>
-                                <h2 class="nobottommargin"><a href="#">Dosa is a popular south Indian food. Do you want to know more about this special food?</a></h2>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 col-sm-12 bottommargin-sm">
+
+                        <div class="ipost clearfix">
+                            <div class="entry">
+                                <div class="entry-image nobottommargin">
+                                    <a href="<?php echo base_url(); ?>assets/theme2/res/images/blogs/2.jpg" data-lightbox="image"><img class="image_fade" src="<?php echo base_url(); ?>assets/theme2/res/images/blogs/2.jpg" alt="Image 1"></a>
+                                </div>
+                                <div class="entry-title">
+                                    <ul class="entry-meta clearfix">
+                                        <li><a href="#" style="text-transform: capitalize;"><?php echo date("dS M Y"); ?></a></li>
+                                        <li><a href="#">ADMIN</a></li>
+                                    </ul>
+                                    <div class="clear"></div>
+                                    <h2 class="nobottommargin"><a href="#">Onion Rava Dosa Recipe
+
+                                        </a>
+                                    </h2>
+                                </div>
+                                <div class="entry-details">
+                                    <p>
+                                        About Onion Rava Dosa Recipe: A crisp dosa made with rava an topped with a onion-green chilly mixture.                                    </p>
+                                    <h3>
+                                        How to Make Onion Rava Dosa
+
+                                    </h3>
+                                    <ul>
+                                       <li> 1.Mix the rice flour and rava with some water and mix till its a smooth batter.</li>
+                                       <li> 2.Add the asafoetida, cumin seeds and salt.</li>
+                                       <li> 3.Cover and keep it in a warm place for a few hours to ferment.</li>
+                                       <li> 4.Mix the chopped onions, green chillies, ginger, cashew nuts and black pepper in a bowl and keep it aside.</li>
+                                       <li> 5.Heat a flat pan (tawa) and grease it with a spoon of oil. Add some water in the dosa batter and make it into a thin batter.</li>
+                                       <li> 6.Pour 1/2 cup of the batter in center of the pan and quickly spread with outward spiral motion to form a thin pancake of about 8 inches in diameter.</li>
+                                      <li>  7.Sprinkle some onion-green chilli mixture on top of it. Dot the edges with little oil and cook until golden brown.</li>
+                                       <li> 8.Using a spatula carefully fold the dosa and remove from the pan. Serve hot with chutneys or sambhar.</li>
+                                    </ul>
+                                    <p>
+                                        <b>Key Ingredients:</b> Rava (semolina), Rice flour, Green chillies, Ginger, Black pepper, Cumin seeds, Asafoetida, Onions, Cashew nuts (broken), Salt, Oil
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class='col-md-6'>
+                    <div class="col-lg-12 col-sm-12 bottommargin-sm">
+
+                        <div class="ipost clearfix">
+                            <div class="entry">
+                                <div class="entry-image nobottommargin">
+                                    <a href="<?php echo base_url(); ?>assets/theme2/res/images/blogs/buttermilk.sambar.jpg" data-lightbox="image"><img class="image_fade" src="<?php echo base_url(); ?>assets/theme2/res/images/blogs/buttermilk.sambar.jpg" alt="Image 1"></a>
+                                </div>
+                                <div class="entry-title">
+                                    <ul class="entry-meta clearfix">
+                                        <li><a href="#" style="text-transform: capitalize;"><?php echo date("dS M Y"); ?></a></li>
+                                        <li><a href="#">ADMIN</a></li>
+                                    </ul>
+                                    <div class="clear"></div>
+                                    <h2 class="nobottommargin"><a href="#">Buttermilk Sambar Recipe
+
+                                        </a>
+                                    </h2>
+                                </div>
+                                <div class="entry-details">
+                                    <p>
+                                        Sambar is one of the most loved dishes in the south Indian cuisine. Lady finger and brinjal cooked in a sambar paste and buttermilk base. A tempting tadka poured over the sambar and served piping hot!                                    
+                                    </p>
+                                    How to Make Buttermilk Sambar
+
+
+                                    <h3> For the vegetables:</h3>
+                                    <ul>
+
+                                        <li>1.In a wok add coconut oil, turmeric powder, red chilli powder. Saute.
+                                        </li>
+                                        <li>2.Add chopped bhindi and a pinch of salt. Add the tamarind extract, mix. Add a little water and cook for 4-5 minutes.
+                                        </li>
+                                        <li> 3.Now add brinjals and cook till the brinjals are in a half cooked stage. Turn off the fire and keep aside.
+                                        </li>
+                                    </ul>
+
+                                    <h3>For the sambar paste:</h3>
+                                    <ul>
+                                        <li> 1.In a pan dry roast urad dal, tuvar dal, cumin seeds, fenugreek seeds, coriander seeds, black pepper, garlic, ginger, coconut, turmeric powder, red chilli powder and sea salt.</li>
+                                        <li> 2.In a mortar and pestle grind the dry roasted spices. Add a little water to facilitate the grinding process. The paste is ready. Keep aside.</li>
+                                    </ul>
+
+                                    <h3>For the base:</h3>
+                                    <ul>
+                                        <li> 1.In a pan add coconut oil and the sambar paste. Add a little water to make the paste smoother. Cook till the oil starts to leave on the sides.</li>
+                                        <li> 2.Add tamarind extract, jaggery and salt. Mix well.</li>
+                                        <li> 3.Now slowly and gradually add the butter milk and keep stirring.</li>
+                                        <li> 4.Add the vegetables. Cover and cook the sambar on low to medium heat till the vegetables are done. Pour over the tadka.</li>
+                                    </ul>
+
+                                    <h3>For the tadka:</h3>
+                                    <ul>
+                                        <li>1.In a small pan add coconut oil, kashmiri red chili, mustard seeds and curry leaves. Let them crackle.</li>
+                                        <li>2.Add a pinch of sea salt and grated coconut. Mix well.</li>
+                                        <li>3.Add to the sambar.</li>
+                                    </ul>
+
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-sm-6 bottommargin-sm">
-                    <div class="ipost clearfix">
-                        <div class="entry">
-                            <div class="entry-image nobottommargin">
-                                <a href="<?php echo base_url(); ?>assets/theme2/res/images/blogs/3.jpg" data-lightbox="image"><img class="image_fade" src="<?php echo base_url(); ?>assets/theme2/res/images/blogs/3.jpg" alt="Image 2"></a>
-                            </div>
-                            <div class="entry-title">
-                                <ul class="entry-meta clearfix">
-                                    <li><a href="#" style="text-transform: capitalize;"><?php echo date("dS M Y");?></a></li>
-                                    <li><a href="#">ADMIN</a></li>
-                                </ul>
-                                <div class="clear"></div>
-                                <h2 class="nobottommargin"><a href="#">Panipuri or Phuchka is a type of snack that originated in the Indian subcontinent.</a></h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-            
+
             </div>
         </div>
         <div class="section mt-3 border-top nobg py-5" style="    padding: 0;display: none;
-    margin: 0;">
+             margin: 0;">
             <div class="container clearfix">
                 <div class="row align-items-center">
                     <div class="col-sm-6 mr-auto">
@@ -87,7 +207,7 @@ $this->load->view('layout/header');
             </div>
         </div>
     </div>
-     <?php
+    <?php
     $this->load->view('layout/contactfooter');
     ?>
     <div style="position: absolute; bottom: 0; left: 0; width: 100%; z-index: 3; background: url('images/sketch-header.png') repeat center bottom; background-size: auto 100%; height: 40px; margin-bottom: -10px;"></div>

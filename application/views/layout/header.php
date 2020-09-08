a1cb84270c6ac4f5dbf9f6028141a2061a537825
dev <dev@develop.com>
2020-09-08 16:45:49 +0530

    changes

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -39,101 +39,120 @@
             /* Page Loader CSS */
             .css3-spinner:before,.pizza .slice:after,.pizza .slice:before{content:''}.css3-spinner{height:100vh;-webkit-box-align:center;-ms-flex-align:center;align-items:center;display:-webkit-box;display:-ms-flexbox;display:flex;-webkit-box-pack:center;-ms-flex-pack:center;justify-content:center;background:#FFF}.css3-spinner:before{position:absolute;top:50%;left:50%;-webkit-transform:translateX(-50%) translateY(-50%);transform:translateX(-50%) translateY(-50%);width:15vmin;height:2vmin;background:#DDD;margin-top:17.5vmin;-webkit-filter:blur(10px);filter:blur(10px);border-radius:100%}.pizza{height:20vmin;width:20vmin;-webkit-box-align:center;-ms-flex-align:center;align-items:center;background:0 0;position:relative;-webkit-animation:rotate 13s linear infinite;animation:rotate 13s linear infinite}@-webkit-keyframes rotate{to{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}@keyframes rotate{to{-webkit-transform:rotate(360deg);transform:rotate(360deg)}}.pizza .slice{z-index:-1;overflow:visible;position:absolute;width:0;height:0;border-style:solid;border-width:10vmin 2.75vmin 0;border-color:#ffdc73 transparent transparent;left:7.5vmin;top:0;-webkit-transform-origin:50% 100%;transform-origin:50% 100%;-webkit-transform:rotate(0);transform:rotate(0);-webkit-animation:loading 1.8125s ease-in-out infinite;animation:loading 1.8125s ease-in-out infinite}@-webkit-keyframes loading{0%,100%,49%{opacity:1}50%,99%{opacity:0}}@keyframes loading{0%,100%,49%{opacity:1}50%,99%{opacity:0}}.pizza .slice:nth-of-type(2n):after{box-shadow:.5vmin 2.5vmin 0 #cc333f}.pizza .slice:nth-of-type(4n):after{box-shadow:.5vmin 2.5vmin 0 #cc333f,1.5vmin 5vmin 0 #cc333f}.pizza .slice:nth-of-type(1){-webkit-transform:rotate(-27.75deg);transform:rotate(-27.75deg);-webkit-animation-delay:-62.5ms;animation-delay:-62.5ms}.pizza .slice:nth-of-type(2){-webkit-transform:rotate(-55.5deg);transform:rotate(-55.5deg);-webkit-animation-delay:-125ms;animation-delay:-125ms}.pizza .slice:nth-of-type(3){-webkit-transform:rotate(-83.25deg);transform:rotate(-83.25deg);-webkit-animation-delay:-.1875s;animation-delay:-.1875s}.pizza .slice:nth-of-type(4){-webkit-transform:rotate(-111deg);transform:rotate(-111deg);-webkit-animation-delay:-.25s;animation-delay:-.25s}.pizza .slice:nth-of-type(5){-webkit-transform:rotate(-138.75deg);transform:rotate(-138.75deg);-webkit-animation-delay:-.3125s;animation-delay:-.3125s}.pizza .slice:nth-of-type(6){-webkit-transform:rotate(-166.5deg);transform:rotate(-166.5deg);-webkit-animation-delay:-375ms;animation-delay:-375ms}.pizza .slice:nth-of-type(7){-webkit-transform:rotate(-194.25deg);transform:rotate(-194.25deg);-webkit-animation-delay:-.4375s;animation-delay:-.4375s}.pizza .slice:nth-of-type(8){-webkit-transform:rotate(-222deg);transform:rotate(-222deg);-webkit-animation-delay:-.5s;animation-delay:-.5s}.pizza .slice:nth-of-type(9){-webkit-transform:rotate(-249.75deg);transform:rotate(-249.75deg);-webkit-animation-delay:-.5625s;animation-delay:-.5625s}.pizza .slice:nth-of-type(10){-webkit-transform:rotate(-277.5deg);transform:rotate(-277.5deg);-webkit-animation-delay:-625ms;animation-delay:-625ms}.pizza .slice:nth-of-type(11){-webkit-transform:rotate(-305.25deg);transform:rotate(-305.25deg);-webkit-animation-delay:-.6875s;animation-delay:-.6875s}.pizza .slice:nth-of-type(12){-webkit-transform:rotate(-333deg);transform:rotate(-333deg);-webkit-animation-delay:-.75s;animation-delay:-.75s}.pizza .slice:before{position:absolute;height:1.5vmin;width:6vmin;background:#bbb083;top:-10.5vmin;left:-3vmin;border-radius:100vmin 100vmin .5vmin .5vmin/50vmin}.pizza .slice:after{border-radius:100%;position:absolute;width:1.25vmin;height:1.25vmin;background:#cc333f;left:-1vmin;top:-7vmin;z-index:2}
         </style>
-    </head>
-    <body ng-app="App" class="stretched sticky-footer" data-loader-html="<span class='loadingimage'><img src='<?php echo base_url(); ?>assets/theme2/res/images/preloader@2x.gif'>'<h2 class='font-secondary'>Loading...</h2> </span>">
+        <!-- Facebook Pixel Code -->
+        <script>
+            !function(f, b, e, v, n, t, s)
+            {if (f.fbq)return; n = f.fbq = function(){n.callMethod?
+                    n.callMethod.apply(n, arguments):n.queue.push(arguments)};
+            if (!f._fbq)f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+            n.queue = []; t = b.createElement(e); t.async = !0;
+            t.src = v; s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)}(window, document, 'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2769320453302463');
+            fbq('track', 'PageView');
+        </script>
+        <noscript>
+    <img height="1" width="1" 
+         src="https://www.facebook.com/tr?id=2769320453302463&ev=PageView
+         &noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
+</head>
+<body ng-app="App" class="stretched sticky-footer" data-loader-html="<span class='loadingimage'><img src='<?php echo base_url(); ?>assets/theme2/res/images/preloader@2x.gif'>'<h2 class='font-secondary'>Loading...</h2> </span>">
 
-        <div id="wrapper" class="clearfix" ng-controller="ShopController" id="ShopController">
-            <script>
+    <div id="wrapper" class="clearfix" ng-controller="ShopController" id="ShopController">
+        <script>
 
 
-                var App = angular.module('App', []).config(function ($interpolateProvider, $httpProvider) {
-                //$interpolateProvider.startSymbol('{$');
-                //$interpolateProvider.endSymbol('$}');
-                $httpProvider.defaults.headers.common = {};
-                $httpProvider.defaults.headers.post = {};
-                });
-                var baseurl = "<?php echo site_url(); ?>";
-                var imageurlg = "<?php echo PRODUCTIMAGELINK; ?>";
-                var globlecurrency = "<?php echo globle_currency; ?>";
-                var avaiblecredits = 0;</script>
-            <header id="header" class="static-sticky transparent-header split-menu clearfix">
-                <div id="header-wrap" style="background-image: url('<?php echo base_url(); ?>assets/theme2/res/images/sketch-header.png');">
-                    <div class="container clearfix">
-                        <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+            var App = angular.module('App', []).config(function ($interpolateProvider, $httpProvider) {
+            //$interpolateProvider.startSymbol('{$');
+            //$interpolateProvider.endSymbol('$}');
+            $httpProvider.defaults.headers.common = {};
+            $httpProvider.defaults.headers.post = {};
+            });
+            var baseurl = "<?php echo site_url(); ?>";
+            var imageurlg = "<?php echo PRODUCTIMAGELINK; ?>";
+            var globlecurrency = "<?php echo globle_currency; ?>";
+            var avaiblecredits = 0;</script>
+        <header id="header" class="static-sticky transparent-header split-menu clearfix">
+            <div id="header-wrap" style="background-image: url('<?php echo base_url(); ?>assets/theme2/res/images/sketch-header.png');">
+                <div class="container clearfix">
+                    <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
 
-                        <div id="logo">
-                            <a href="<?php echo site_url("/"); ?>" class="standard-logo"><img src="<?php echo base_url(); ?>assets/images/logo.png" alt="Woodlandshk Logo"></a>
-                            <a href="#" class="retina-logo"><img src="<?php echo base_url(); ?>assets/images/logo.png" alt="Woodlandshk Logo"></a>
-                        </div>
+                    <div id="logo">
+                        <a href="<?php echo site_url("/"); ?>" class="standard-logo"><img src="<?php echo base_url(); ?>assets/images/logo.png" alt="Woodlandshk Logo"></a>
+                        <a href="#" class="retina-logo"><img src="<?php echo base_url(); ?>assets/images/logo.png" alt="Woodlandshk Logo"></a>
+                    </div>
 
-                        <nav id="primary-menu" class="with-arrows clearfix">
-                            <ul>
-                                <li><a href="<?php echo site_url("about-us"); ?>"><div>INSIGHTS</div></a></li>
-                                <!--<li><a href="<?php echo site_url("menu/0/0"); ?>"><div>Menu</div></a></li>-->
+                    <nav id="primary-menu" class="with-arrows clearfix">
+                        <ul>
+                            <li><a href="<?php echo site_url("about-us"); ?>"><div>INSIGHTS</div></a></li>
+                            <!--<li><a href="<?php echo site_url("menu/0/0"); ?>"><div>Menu</div></a></li>-->
 
-                                <li><a href="<?php echo site_url("gallery"); ?>"><div>Gallery</div></a></li>
+                            <li><a href="<?php echo site_url("gallery"); ?>"><div>Gallery</div></a></li>
 
-                                <li><a href="<?php echo site_url("loyalty-program"); ?>"><div>REWARD</div></a></li>
+                            <li><a href="<?php echo site_url("loyalty-program"); ?>"><div>REWARD</div></a></li>
 
-                                <li><a href="<?php echo site_url("review"); ?>"><div>Review</div></a></li>
-                                <!--<li><a href="<?php echo site_url("blog"); ?>"><div>Blog</div></a></li>-->
-                                <li><a href="<?php echo site_url("feedback"); ?>" class=""><div class="">Feedback </div></a></li>
-                                <li><a href="<?php echo site_url("annual-charity"); ?>"><div>  CHARITY</div></a></li>
+                            <li><a href="<?php echo site_url("review"); ?>"><div>Review</div></a></li>
+                            <!--<li><a href="<?php echo site_url("blog"); ?>"><div>Blog</div></a></li>-->
+                            <li><a href="<?php echo site_url("feedback"); ?>" class=""><div class="">Feedback </div></a></li>
+                            <li><a href="<?php echo site_url("annual-charity"); ?>"><div>  CHARITY</div></a></li>
 
-                            </ul>
-                            <ul>
-                                <li><a href="<?php echo site_url("contact"); ?>"><div>Contact</div></a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="<?php echo site_url("contact"); ?>"><div>Contact</div></a></li>
 <!--                                <li><a href="<?php echo site_url("feedback"); ?>" class=""><div class="">Feedback </div></a></li>-->
 
-                                <li>
-                                    <?php
-                                    $checktime = $this->Utils->checkTime(date("h:i A"));
-                                    if ($checktime['code']) {
-                                        ?>
-                                        <a href="<?php echo site_url("menu/0/0"); ?>" class="reservation_buttons "><div>Order Now</div></a>
-                                        <?php
-                                    } else {
-                                        ?>
-                                        <a href="#" class="reservation_buttons disablebooking"><div>Order Now</div></a>
-
-                                        <?php
-                                        echo "<span class='smallmessage'>" . $checktime['message'] . "</span>";
-                                    }
+                            <li>
+                                <?php
+                                $checktime = $this->Utils->checkTime(date("h:i A"));
+                                if ($checktime['code']) {
                                     ?>
-                                </li>
-                                <li><a href="<?php echo site_url("our-menu/0/0"); ?>" class="reservation_buttons "><div class="">MENU </div></a></li>
+                                    <a href="<?php echo site_url("menu/0/0"); ?>" class="reservation_buttons "><div>Order Now</div></a>
+                                    <?php
+                                } else {
+                                    ?>
+                                    <a href="#" class="reservation_buttons disablebooking"><div>Order Now</div></a>
 
-                                <li><a href="<?php echo site_url("book-now"); ?>" class="reservation_buttons "><div class="">Book </div></a></li>
+                                    <?php
+                                    echo "<span class='smallmessage'>" . $checktime['message'] . "</span>";
+                                }
+                                ?>
+                            </li>
+                            <li><a href="<?php echo site_url("our-menu/0/0"); ?>" class="reservation_buttons "><div class="">MENU </div></a></li>
 
-                                <li>
-                                    <a href="<?php echo site_url("cart") ?>" class="cartheadericon">
-                                        <i class="icon-et-basket" style="    font-size: 20px;"></i><span>{{globleCartData.total_quantity}}</span>
+                            <li><a href="<?php echo site_url("book-now"); ?>" class="reservation_buttons "><div class="">Book </div></a></li>
+
+                            <li>
+                                <a href="<?php echo site_url("cart") ?>" class="cartheadericon">
+                                    <i class="icon-et-basket" style="    font-size: 20px;"></i><span>{{globleCartData.total_quantity}}</span>
+                                </a>
+                            </li>
+                            <li>
+                                <?php
+                                $session_data = $this->session->userdata('logged_in');
+                                if ($session_data) {
+                                    ?>
+
+                                    <a href="<?php echo site_url("Account/profile") ?>" class="cartheadericon" style="    padding-left: 24px;">
+                                        <i class="icon-et-profile-male" style="    font-size: 20px;"></i> 
                                     </a>
-                                </li>
-                                <li>
                                     <?php
-                                    $session_data = $this->session->userdata('logged_in');
-                                    if ($session_data) {
-                                        ?>
-
-                                        <a href="<?php echo site_url("Account/profile") ?>" class="cartheadericon" style="    padding-left: 24px;">
-                                            <i class="icon-et-profile-male" style="    font-size: 20px;"></i> 
-                                        </a>
-                                        <?php
-                                    } else {
-                                        ?>
-                                        <a href="<?php echo site_url("Account/login") ?>" class="cartheadericon" style="    padding-left: 24px;">
-                                            <i class="icon-et-lock" style="    font-size: 20px;"></i> 
-                                        </a>
-                                        <?php
-                                    }
+                                } else {
                                     ?>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
+                                    <a href="<?php echo site_url("Account/login") ?>" class="cartheadericon" style="    padding-left: 24px;">
+                                        <i class="icon-et-lock" style="    font-size: 20px;"></i> 
+                                    </a>
+                                    <?php
+                                }
+                                ?>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-            </header>
+            </div>
+        </header>
 
 
 

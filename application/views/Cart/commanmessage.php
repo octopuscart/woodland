fd58039447f2ece71bd7aec9a17af3d424635891
octopuscart <dev@develop.com>
2021-04-12 23:40:01 +0530

    cahgsn

--- application/views/Cart/commanmessage.php
+++ application/views/Cart/commanmessage.php
@@ -1,16 +1,18 @@
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
     <div class="card card-default ">
         <div class="card-heading change-color-gradiant" role="tab" id="headingOne" >
-            <h4 class="card-title " style="font-size: 16px; margin: 7px 0px;  margin: 0; text-align: center; color: white;">Delivery process is only available in Tsim Sha Tsui and Whampoa Garden
+            <h4 class="card-title " style="font-size: 16px; margin: 7px 0px;  margin: 0; text-align: center; color: white;">
+                <!--Delivery process is only available in Tsim Sha Tsui and Whampoa Garden-->
+                Delivery process is only available in Tsim Sha Tsui 
                 <table class='tablemessageblock'>
                     <tr>
                         <td rowspan="2"> Delivery Charges:</td>
                         <td>Free Delivery In Tsim Sha Tsui</td>
                     </tr>
-                    <tr>
+<!--                    <tr>
 
                         <td>Whampoa Garden ($40 On Order value < $400)</td>
-                    </tr>
+                    </tr>-->
                 </table>
 
                 

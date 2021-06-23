0eb7c81ddc830108fe7ae7bb899a45f89db7b7c4
octopuscart <dev@develop.com>
2021-06-23 17:09:24 +0530

    changs

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -123,7 +123,7 @@
                             </li>
                             <!--<li><a href="<?php echo site_url("our-menu/0/0"); ?>" class="reservation_buttons " ><div class="">MENU </div></a></li>-->
 
-                            <li><a href="<?php echo site_url("book-now"); ?>" class="reservation_buttons "><div class="">Reservation </div></a></li>
+                            <li><a href="https://book.bistrochat.com/woodlands" target="_blank" class="reservation_buttons "><div class="">Reservation </div></a></li>
 
                             <li>
                                 <a href="<?php echo site_url("cart") ?>" class="cartheadericon">

f1a53980ed190da58714f85b897322c4feba0aed
octopuscart <octopuscartltd@gmail.com>
2022-12-22 07:59:35 +0530

    changes

--- application/views/Product/productListFood.php
+++ application/views/Product/productListFood.php
@@ -30,95 +30,98 @@ $this->load->view('layout/header');
     <div class="content-wrap nopadding">
         <div class="container">
             <?php
-            if($location == "tst"){
-            ?>
-            <div class="tabs tabs-justify clear-bottommargin clearfix" id="tab-1">
-                <ul class="tab-nav clearfix border-bottom-0">
-                    <?php
-                    foreach ($categories as $catkey => $catvalue) {
-                        ?>
-                        <li style="    border: none;"> <a href="#tabs-foodmenu-<?php echo $catvalue['id']; ?>">
-                                <img src="<?php echo base_url(); ?>assets/theme2/res/food/default.png" style="background: url(<?php echo base_url(); ?>assets/theme2/res/food/<?php echo $categories2[$catvalue['id']]; ?>);border:none;height:50px;width:50px;border-radius: 50%!important;" alt="1" class="rounded product-image-back rounded_border">
-                                <br/>
-                                <?php echo $catvalue['category_name']; ?>
-                            </a></li>
-                    <?php } ?>
-                </ul>
-                <div class="tab-container mt-4">
+            if ($location == "tst") {
+                ?>
+                <div class="tabs tabs-justify clear-bottommargin clearfix" id="tab-1">
+                    <ul class="tab-nav clearfix border-bottom-0">
+                        <?php
+                        foreach ($categories as $catkey => $catvalue) {
+                            ?>
+                            <li style="    border: none;"> <a href="#tabs-foodmenu-<?php echo $catvalue['id']; ?>">
+                                    <img src="<?php echo base_url(); ?>assets/theme2/res/food/default.png" style="background: url(<?php echo base_url(); ?>assets/theme2/res/food/<?php echo $categories2[$catvalue['id']]; ?>);border:none;height:50px;width:50px;border-radius: 50%!important;" alt="1" class="rounded product-image-back rounded_border">
+                                    <br/>
+                                    <?php echo $catvalue['category_name']; ?>
+                                </a></li>
+                        <?php } ?>
+                    </ul>
+                    <div class="tab-container mt-4">
 
-                    <?php
-                    foreach ($categories as $catkey => $catvalue) {
-                        ?>
-                        <div class="tab-content clearfix" id="tabs-foodmenu-<?php echo $catvalue['id']; ?>">
-                            <div class="row clearfix">
-                                <div class="row col-md-12">
-                                    <h1 class="font-secondary capitalize ls0" style="font-size: 50px;text-align: center;    text-align: center!important;
-                                        width: 100%;">   <?php echo $catvalue['category_name']; ?></h1>
-                                </div>
-                                <?php
-                                $categoryproducts = $productlist[$catvalue['id']];
-                                foreach ($categoryproducts as $prkey => $prvalue) {
-                                    ?>
-
-                                    <div class="col-lg-4 col-md-6" >
-                                        <div class="iportfolio mb-4 clearfix">
-                                            <a href="#"  class="portfolio-image12 hoverdesription">
-                                                <span>
-                                                    <?php echo $prvalue['short_description']; ?>
-                                                    <br/>
-
-                                                    <?php echo $prvalue['description']; ?>
-                                                </span>
-                                                <img src="<?php echo base_url(); ?>assets/theme2/res/food/default2.png"  alt="1" class="rounded product-image-back">
-
-                                            </a>
-                                            <a href="#" ng-click="addToCart(<?php echo $prvalue['id']; ?>, 1)" class="portfolio-image12">
-                                                <img src="<?php echo base_url(); ?>assets/theme2/res/food/default.png" style="background: url(<?php echo base_url(); ?>assets/theme2/res/food/<?php echo $prvalue['file_name']; ?>)" alt="1" class="rounded product-image-back">
-                                            </a>
-
-                                            <div class="portfolio-desc pt-2">
-                                                <h4 class="mb-1"><a href="#" class="" ><?php echo $prvalue['title']; ?></a></h4>
-                                                <div class="item-price">{{<?php echo $prvalue['price']; ?>|currency:"<?php echo globle_currency; ?> "}}</div>
-                                            </div>
-                                            <?php
-                                            if ($viewonly == '0') {
-                                                ?>
-                                                <div class="">
-                                                    <button ng-click="addToCart(<?php echo $prvalue['id']; ?>, 1)" class="button button-circle button-light text-white button_product  colordarkgreen ">
-                                                        Add To Cart
-                                                    </button>
-
-                                                    <button ng-click="addToBuy(<?php echo $prvalue['id']; ?>, 1)" class="button button-circle button-light text-white button_product  colorlightyellow " style="    float: right;">
-                                                        Buy Now
-                                                    </button>
-                                                </div>
-                                                <?php
-                                            }
-                                            ?>
-                                        </div>
+                        <?php
+                        foreach ($categories as $catkey => $catvalue) {
+                            ?>
+                            <div class="tab-content clearfix" id="tabs-foodmenu-<?php echo $catvalue['id']; ?>">
+                                <div class="row clearfix">
+                                    <div class="row col-md-12">
+                                        <h1 class="font-secondary capitalize ls0" style="font-size: 50px;text-align: center;    text-align: center!important;
+                                            width: 100%;">   <?php echo $catvalue['category_name']; ?></h1>
                                     </div>
                                     <?php
-                                }
-                                ?>
+                                    $categoryproducts = $productlist[$catvalue['id']];
+                                    foreach ($categoryproducts as $prkey => $prvalue) {
+                                        ?>
+
+                                        <div class="col-lg-4 col-md-6" >
+                                            <div class="iportfolio mb-4 clearfix">
+                                                <a href="#"  class="portfolio-image12 hoverdesription">
+                                                    <span>
+                                                        <?php echo $prvalue['short_description']; ?>
+                                                        <br/>
+
+                                                        <?php echo $prvalue['description']; ?>
+                                                    </span>
+                                                    <img src="<?php echo base_url(); ?>assets/theme2/res/food/default2.png"  alt="1" class="rounded product-image-back">
+
+                                                </a>
+                                                <a href="#" ng-click="addToCart(<?php echo $prvalue['id']; ?>, 1)" class="portfolio-image12">
+                                                    <img src="<?php echo base_url(); ?>assets/theme2/res/food/default.png" style="background: url(<?php echo base_url(); ?>assets/theme2/res/food/<?php echo $prvalue['file_name']; ?>)" alt="1" class="rounded product-image-back">
+                                                </a>
+
+                                                <div class="portfolio-desc pt-2">
+                                                    <h4 class="mb-1"><a href="#" class="" ><?php echo $prvalue['title']; ?></a></h4>
+                                                    <div class="item-price">{{<?php echo $prvalue['price']; ?>|currency:"<?php echo globle_currency; ?> "}}</div>
+                                                </div>
+                                                <?php
+                                                if ($viewonly == '0') {
+                                                    ?>
+                                                    <div class="">
+                                                        <button ng-click="addToCart(<?php echo $prvalue['id']; ?>, 1)" class="button button-circle button-light text-white button_product  colordarkgreen ">
+                                                            Add To Cart
+                                                        </button>
+
+                                                        <button ng-click="addToBuy(<?php echo $prvalue['id']; ?>, 1)" class="button button-circle button-light text-white button_product  colorlightyellow " style="    float: right;">
+                                                            Buy Now
+                                                        </button>
+                                                    </div>
+                                                    <?php
+                                                }
+                                                ?>
+                                            </div>
+                                        </div>
+                                        <?php
+                                    }
+                                    ?>
+                                </div>
                             </div>
-                        </div>
-                        <?php
-                    }
-                    ?>
+                            <?php
+                        }
+                        ?>
+
+                    </div>
+                </div>
+                <?php
+            } else {
+                ?>
+                <div class="well well-sm mt-5 mb-5 row">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                        <h2 class="text-center">
+                            <img class="img-thumbnail" src="<?php echo base_url(); ?>assets/locations/commingSoon.jpg" alt="Wan Chai Location" style="border-radius: 20px;" />
+                        </h2>
+                    </div>
+                    <div class="col-md-3"></div>
 
                 </div>
-            </div>
-            <?php
-            }
-            else{
-                ?>
-            <div class="well well-sm mt-5 mb-5">
-                <h2 class="text-center">
-                      <span class="t400">Coming Soon</span>
-                </h2>
-                
-            </div>
-                    <?php
+                <?php
             }
             ?>
         </div>
@@ -160,13 +163,13 @@ $this->load->view('layout/header');
                 </div>
             </div>
         </div>
-<!--        <div class="section nomargin nobg" style="padding: 80px 0 70px;">
-            <div class="container">
-                <div class="divcenter d-flex justify-content-center center" style="max-width: 900px;">
-                    <h3 class="mb-0 ls0">Free Delivery On Order Value $400 And Up <a href="#" class="button button-circle button-xlarge button-light text-white ls0 nott mt-0 mb-1 ml-3 colorlightgreen" style="position: relative;"><span>Order Now</span> <i class="icon-line-arrow-right t600"></i></a></h3>
-                </div>
-            </div>
-        </div>-->
+        <!--        <div class="section nomargin nobg" style="padding: 80px 0 70px;">
+                    <div class="container">
+                        <div class="divcenter d-flex justify-content-center center" style="max-width: 900px;">
+                            <h3 class="mb-0 ls0">Free Delivery On Order Value $400 And Up <a href="#" class="button button-circle button-xlarge button-light text-white ls0 nott mt-0 mb-1 ml-3 colorlightgreen" style="position: relative;"><span>Order Now</span> <i class="icon-line-arrow-right t600"></i></a></h3>
+                        </div>
+                    </div>
+                </div>-->
     </div>
     <div style="position: absolute; bottom: 0; left: 0; width: 100%; z-index: 3; background: url('<?php echo base_url(); ?>assets/theme2/res/images/sketch-header.png') repeat center bottom; background-size: auto 100%; height: 40px; margin-bottom: -10px;"></div>
 </section>
